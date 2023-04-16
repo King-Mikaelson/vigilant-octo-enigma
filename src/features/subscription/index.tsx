8c6f4562f9239dccb33b1c995f7f46cdf4c9ddbc
Bright Iortsor <iortsor@gmail.com>
2023-04-16 19:39:51 +0100

    FEAT/added Subscription Page

--- src/features/subscription/index.tsx
+++ src/features/subscription/index.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-import Button from "../../components/ui/button";
 import { BsPerson } from "react-icons/bs";
 import { FiCreditCard } from "react-icons/fi";
 import { RiCalendarCheckFill } from "react-icons/ri";
 import { MdLockOutline } from "react-icons/md";
 import { AiFillCheckCircle } from "react-icons/ai";
+import Logo from "../../assets/uppistLogo.png";
 
 export default function Subscribe() {
   return (
@@ -13,16 +13,16 @@ export default function Subscribe() {
         <div className="sub-header">
           <h3>The Right Plan for Your Business</h3>
           <p>
-            We’ll be taking a <span className="percent">4%</span> commission of
-            your monthly sales. This way you don’t have to bother about a
-            particular subscription amount every month, we take just{" "}
-            <span>4%</span> of whatever you make monthly.
+            We’ll be taking a <span>4%</span> commission of your monthly sales.
+            This way you don’t have to bother about a particular subscription
+            amount every month, we take just <span>4%</span> of whatever you
+            make monthly.
           </p>
         </div>
         <div className="sub-content">
           {/* Perks */}
           <div className="perks-container">
-            <p>PERKS</p>
+            <div className="perks-header">PERKS</div>
             <div className="sub-trial">
               <span>
                 <AiFillCheckCircle />
@@ -109,19 +109,21 @@ export default function Subscribe() {
                 </div>
               </div>
 
-              <Button
-                text="Start Free Trial"
-                onclick={(e) => e.preventDefault()}
-              />
+              <button className="trial-btn">Start Free Trial</button>
             </form>
 
-            <Link to="/" className="auth__links">
+            <Link to="/" className="sub-link">
               Cancel
             </Link>
           </div>
         </div>
       </div>
-      <span>Powered By Uppist</span>
+      <div className="logo-div">
+        <span>Powered By</span>{" "}
+        <span>
+          <img src={Logo} alt="logo" className="logo-img" />
+        </span>
+      </div>
     </main>
   );
 }
