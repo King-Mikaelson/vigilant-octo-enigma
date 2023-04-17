5f2a3574e4c945e2b966751083c3afc279d4d5a8
King-Mikaelson <anazodomichael27@gmail.com>
2023-04-17 20:31:09 +0100

    Started work on Dashboard and Menu Page

---
+++ src/components/topBar/index.tsx
@@ -0,0 +1,24 @@
+import Input from "../ui/Input";
+import { CiSearch } from "react-icons/ci";
+
+type Props = {};
+
+function TopBar({}: Props) {
+    const current = new Date();
+    const date = `${current.toLocaleString("en-US", {
+      weekday: "long",
+    })}, ${current.toLocaleString("en-US", {
+      month: "long",
+    })} ${current.getDate()}, ${current.getFullYear()}`;
+  return (
+    <div className="topbar__container">
+        <div className="search__container">
+        <CiSearch size={20} />
+        <Input type="text" />
+        </div>
+        <p>{date}</p>
+    </div>
+  );
+}
+
+export default TopBar;
