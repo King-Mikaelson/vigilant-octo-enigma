033109cc5d1512f025539439d5500332abe0b1f6
King-Mikaelson <anazodomichael27@gmail.com>
2023-04-19 15:47:52 +0100

    made changes to dashboard scrrens

--- src/styleGuides.tsx
+++ src/styleGuides.tsx
@@ -1,9 +1,9 @@
 import Pos from "./features/pos";
 import Subscribe from "./features/subscription";
 import TopBar from "./components/topBar/index";
-import DeleteItem from "./components/items/deleteItem";
+// import DeleteItem from "./components/items/deleteItem";
 // import EditItem from "./components/items/editItem";
-import AddItem from "./components/items/addItem";
+// import AddItem from "./components/items/addItem";
 import Filters from "./components/filters";
 import MenuCard from "./components/menuCard";
 
@@ -13,10 +13,10 @@ export default function StyleGuides() {
       <MenuCard/>
       <Filters />
       <Pos />
-      <DeleteItem />
+      {/* <DeleteItem /> */}
       {/* Commented this out because the background effect affects the ENTIRE screen */}
       {/* <EditItem /> */}
-      <AddItem />
+      {/* <AddItem /> */}
       <TopBar />
       <Subscribe />
     </div>
