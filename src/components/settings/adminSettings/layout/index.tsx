197ca01700cc5dac45ad11804a2a0a8d01f2d542
Bright Iortsor <iortsor@gmail.com>
2023-04-22 16:46:20 +0100

    added Dashboard layout to Settings page

--- src/components/settings/adminSettings/layout/index.tsx
+++ src/components/settings/adminSettings/layout/index.tsx
@@ -3,6 +3,7 @@ import { CgProfile } from "react-icons/cg";
 import { MdAddCard } from "react-icons/md";
 import { TbUsers } from "react-icons/tb";
 import { NavLink } from "react-router-dom";
+import DashboardLayout from "../../../../layout/dashboardLayout";
 
 interface SettingsLayoutProps {
   children: React.ReactNode;
@@ -16,42 +17,44 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
   };
   return (
     <>
-      <div className="settings-layout">
-        <div className="settings-sidebar">
-          <NavLink
-            to="/settings"
-            className={selectedItem === "profile" ? "selected" : ""}
-            onClick={() => handleItemClick("profile")}
-          >
-            <span className="layout-icon">
-              <CgProfile />
-            </span>
-            Profile
-          </NavLink>
+      <DashboardLayout>
+        <div className="settings-layout">
+          <div className="settings-sidebar">
+            <NavLink
+              to="/settings"
+              className={selectedItem === "profile" ? "selected" : ""}
+              onClick={() => handleItemClick("profile")}
+            >
+              <span className="layout-icon">
+                <CgProfile />
+              </span>
+              Profile
+            </NavLink>
 
-          <NavLink
-            to="/settings/manageusers"
-            className={selectedItem === "users" ? "selected" : ""}
-            onClick={() => handleItemClick("users")}
-          >
-            <span className="layout-icon">
-              <TbUsers />
-            </span>
-            Manage Users
-          </NavLink>
-          <NavLink
-            to="/settings/managesubscriptions"
-            className={selectedItem === "subscriptions" ? "selected" : ""}
-            onClick={() => handleItemClick("subscriptions")}
-          >
-            <span className="layout-icon">
-              <MdAddCard />
-            </span>
-            Manage Subscriptions
-          </NavLink>
+            <NavLink
+              to="/settings/manageusers"
+              className={selectedItem === "users" ? "selected" : ""}
+              onClick={() => handleItemClick("users")}
+            >
+              <span className="layout-icon">
+                <TbUsers />
+              </span>
+              Manage Users
+            </NavLink>
+            <NavLink
+              to="/settings/managesubscriptions"
+              className={selectedItem === "subscriptions" ? "selected" : ""}
+              onClick={() => handleItemClick("subscriptions")}
+            >
+              <span className="layout-icon">
+                <MdAddCard />
+              </span>
+              Manage Subscriptions
+            </NavLink>
+          </div>
+          <div className="settings-content">{children}</div>
         </div>
-        <div className="settings-content">{children}</div>
-      </div>
+      </DashboardLayout>
     </>
   );
 }
